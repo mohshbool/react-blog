d71739c2f53f9a1493cb25151290d6c41d5ac863
Minter27 <mohammad.alshbool2002@gmail.com>
2019-03-12 23:44:17 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Button, Navbar, Nav, Card } from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container fluid>
+        <Navbar bg="primary">
+          <Navbar.Brand href="#home">Blog</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Posts</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Container>
+          <Card>
+            <Card.Body>
+              <Card.Text>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, quibusdam pariatur dicta ipsa aliquid sed! Quaerat nisi veniam similique, iste aspernatur repudiandae, rerum ipsam facere sapiente perferendis obcaecati perspiciatis! Enim.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Container>
+      </Container>
     );
   }
 }
